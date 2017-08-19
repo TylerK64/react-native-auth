768c57ad8602a01096b7379478b8411ef2880a51
Andrea Scott <pair+aazevedojr.icedcoffeeallday@devbootcamp.com>
2017-08-19 12:29:18 -0700

    Create basic layout/framework for LoginForm component. Add firebase dependancy for user auth.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBg8a2iHcyzkv4-h17sVfItBr6ijNFtTs0',
+      authDomain: 'auth-a48b6.firebaseapp.com',
+      databaseURL: 'https://auth-a48b6.firebaseio.com',
+      projectId: 'auth-a48b6',
+      storageBucket: 'auth-a48b6.appspot.com',
+      messagingSenderId: '332217142757'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     );
   }

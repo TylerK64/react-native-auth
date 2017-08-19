82d97bce0d60677b0be2ca58d79afc27aea7cc18
Andrea Scott <pair+aazevedojr.icedcoffeeallday@devbootcamp.com>
2017-08-19 13:50:09 -0700

    Add password InputField component and add secureTextEntry attribute to hide password field input.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, InputField } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <InputField
-            label="Email:"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <InputField
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            secureTextEntry  //can omit setting value for boolean attributes/props; default == true
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
--- src/components/common/InputField.js
+++ src/components/common/InputField.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const InputField = ({ label, value, onChangeText }) => {
+const InputField = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        placeholder={placeholder}
+        autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
       />
     </View>
   )
@@ -23,12 +26,12 @@ const styles = {
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23, //line between each line of text
-    flex: 2
+    flex: 7
   },
   labelStyle: {
     fontSize: 18,
     paddingLeft: 20,
-    flex: 1
+    flex: 3
   },
   containerStyle: {
     height: 40,
